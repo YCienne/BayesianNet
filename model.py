7d8f69e87868e24727578c75d1160ea13b247183
YCienne <lucienneawoudi23@gmail.com>
2024-07-28 19:48:09 +0000

    A

---
+++ model.py
@@ -0,0 +1,11 @@
+from pomegranate import *
+from torch import Node
+
+#Rain node has no parent
+rain = Node(DiscreteDistribution({
+    "none": 0.7,
+    "light": 0.2,
+    "heavy": 0.1
+}), name="rain")
+
+#Track maintenance node is conditional on rain
